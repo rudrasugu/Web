95d95d45c90477961b84b68bfc5901c156c876cb
rudrasugu <rudra.nadarajah@outlook.com>
2025-08-29 19:05:15 +0200

    oppdatert kode

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function endreTekst() {
+    const tekstElement = document.getElementById("tekst");
+    tekstElement.textContent = "Du klikket på knappen! Teksten er nå endret!";
+}
+
+function lukkSide() {
+    window.close();
+}
